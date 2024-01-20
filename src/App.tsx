b2ac5f8aba977f44cca3cf5972dc592c6b579840
nolood <a9514548799@gmail.com>
2024-01-20 17:17:07 +0500

    feat: eslint lint-staged test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "~/shared/ui/kit/ui/button";
 import { type FC } from "react";
 import "./app/styles/globals.css";
 
-const App: FC = () => {
+const App = () => {
   return (
     <div>
       <Button>dsdas</Button>
