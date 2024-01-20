e00203bca85ea81bbed728de6b793bab7de2cdb3
nolood <a9514548799@gmail.com>
2024-01-20 21:24:55 +0500

    feat: test

--- src/shared/ui/layouts/protected-layout/ui/protected-layout.tsx
+++ src/shared/ui/layouts/protected-layout/ui/protected-layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const ProtectedLayout: FC = () => {
   return (
     <div>
-      ProtectedLayout
+      ProtectedLayoutd
       <Outlet />
     </div>
   );
