baef68959060824c94b04d716b13fb679abd0f45
nolood <a9514548799@gmail.com>
2024-01-20 21:24:40 +0500

    feat: test

--- src/shared/lib/cn/cn.test.ts
+++ src/shared/lib/cn/cn.test.ts
@@ -1,5 +1,5 @@
 import { cn } from "./cn.ts";
 
 test("tailwind merge + clsx (CN FUNC)", () => {
-  expect(cn("test", "test2", "kek1")).toBe("test test2 kek1");
+  expect(cn("test", "test2", "kek2")).toBe("test test2 kek2");
 });
